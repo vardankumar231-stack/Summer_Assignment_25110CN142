47798ed307435e41cecb2adc7091bd39facaf009
vardankumar231-stack <vardankumar231@gmail.com>
2026-06-30 19:39:48 +0530

    Day_18

---
+++ Day_18/QUESTION_4.c
@@ -0,0 +1,35 @@
+//write a program to sort array in descending order
+
+#include <stdio.h>
+
+int main() {
+    int n, i, j, temp;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Sort in descending order
+    for(i = 0; i < n - 1; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(arr[i] < arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    printf("Array in descending order:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
